2b2788508ac49eeabea177cb2fdf4ecf2434625e
sulochana223 <sulochana2296@gmail.com>
2019-07-22 11:44:59 +0530

    Test

--- src/main/java/org/cucumber/com/Cucumber65/TestRunner.java
+++ src/main/java/org/cucumber/com/Cucumber65/TestRunner.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "C:\\Users\\sreenivasan\\eclipse-workspace\\Cucumber65\\Feature", glue = "org.cucumber.com.Cucumber65",plugin={"html:target", "Json:target\report.Json"})
+@CucumberOptions(features = "C:\\Users\\sreenivasan\\eclipse-workspace\\Cucumber65\\Feature", glue = "org.cucumber.com.Cucumber65",plugin={"html:target","json:target\report.json"})
 public class TestRunner {
 
 }
